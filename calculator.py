4deedc2d27cce4b4d7afe7418bffd1e7b1e8f3a9
Oleksandr Piddubnyi <sasharomanov0107@gmail.com>
2024-03-04 19:45:00 +0200

    add_gcd

--- calculator.py
+++ calculator.py
@@ -4,3 +4,8 @@ class Calculator():
         self.b=b
     def exp(self):
         return  self.a ** self.b
+    def gcd(self):
+        x, y = abs(self.a), abs(self.b)
+        while y != 0:
+            x, y = y, x % y
+        return x
--- test_.py
+++ test_.py
@@ -18,5 +18,20 @@ class TestCalculator(unittest.TestCase):
         calc = Calculator(-2, -3)
         result = calc.exp()
         self.assertEqual(result, -0.125)
+    def test_gcd_positive_numbers(self):
+        calc = Calculator(54, 24)
+        result = calc.gcd()
+        self.assertEqual(result, 6)
+
+    def test_gcd_negative_numbers(self):
+        calc = Calculator(-54, 24)
+        result = calc.gcd()
+        self.assertEqual(result, 6)
+
+    def test_gcd_mixed_numbers(self):
+        calc = Calculator(-54, -24)
+        result = calc.gcd()
+        self.assertEqual(result, 6)
 if __name__ == '__main__':
     unittest.main()
+    
